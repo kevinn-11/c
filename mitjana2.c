be144210bccc705da564f4c663b253a503021851
kevinn-11 <kevcrenav@alu.edu.gva.es>
2025-10-03 12:31:05 +0200

    Add files via upload

---
+++ mitjana2.c
@@ -0,0 +1,14 @@
+/**************************************
+ * Programa que calcula la mitjana
+ * aritmètica de 2 enters
+ * Kevin Crespo 25-26 *****/
+#include <stdio.h>
+void main(){
+    int a, b, mitjana;
+    printf("1r núm?: ");
+    scanf("%d", &a);
+    printf("2n núm?: ");
+    scanf("%d", &b);
+    printf("La mitjana és: %d", (a+b)/2);
+    return;
+}
