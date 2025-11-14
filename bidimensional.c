5fd0df98601841acda2febdaa19a3233601e18dd
kevinn-11 <kevcrenav@alu.edu.gva.es>
2025-11-14 13:00:01 +0100

    Add files via upload

---
+++ bidimensional.c
@@ -0,0 +1,15 @@
+ /***********************************
+ * Escriu una matriu bidimensional *
+ * Kevin Crespo - 1BAT - 25-26 *
+ * *********************************/
+# include <stdio.h>
+ void main(){
+    int i, j;
+    for(i=1;i<=10;i++)
+        {
+        for(j=1;j<=10;j++)
+            printf("%d, ",i);
+        printf("\n");
+        }
+    return;
+ }
